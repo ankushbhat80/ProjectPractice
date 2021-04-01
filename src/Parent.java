692b723836659e095a337e7823dae47f09acbb29
Ankush <ankushbhat80@gmail.com>
2021-04-01 14:24:39 +0530

    Second commit

---
+++ src/Parent.java
@@ -0,0 +1,14 @@
+
+public class Parent {
+	Parent(){
+		System.out.println("THis inside const of parent:"+this);
+	}
+	//Method overloading
+	public void start() {
+		run();
+	}public void run() {
+		System.out.println("This inside run of parent:"+this);
+		
+	}
+
+}
