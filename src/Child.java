6ce60f56019ce2500195c2cb473dff428a74e784
Ankush <ankushbhat80@gmail.com>
2021-04-01 14:22:03 +0530

    First commit

---
+++ src/Child.java
@@ -0,0 +1,21 @@
+
+public class Child extends Parent{
+	
+	public Child() {
+		System.out.println("This inside cons of Child:"+this);
+	}
+	/*public void run() {
+		//super.run();
+		System.out.println("Child run"+this);
+	}*/
+
+	public static void main(String[] args) {
+		Parent p = new Parent();
+		Child c = new Child();
+		c.start();
+		p.start();
+		
+
+	}
+
+}
